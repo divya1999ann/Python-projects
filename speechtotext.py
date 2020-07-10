a2a088afb965732a4e4218965edb9f2da8a7cb77
Divya Ann Kurien <divya.kurien@gmail.com>
2020-07-11 00:02:03 +0530

    Sppechtotext added

---
+++ speechtotext.py
@@ -0,0 +1,17 @@
+import speech_recognition as SRG 
+import time
+ 
+store = SRG.Recognizer()
+with SRG.Microphone() as s:
+	print("Speak something")
+    audio_input = store.record(s, duration=7)
+    print("Recording time:",time.strftime("%I:%M:%S"))
+    try:
+        text_output = store.recognize_google(audio_input)
+        print("Text converted from audio:\n")
+        print(text_output)
+        print("Ended...")
+ 
+        print("Execution time:",time.strftime("%I:%M:%S"))
+    except:
+           print("Couldn't process the audio input.")
